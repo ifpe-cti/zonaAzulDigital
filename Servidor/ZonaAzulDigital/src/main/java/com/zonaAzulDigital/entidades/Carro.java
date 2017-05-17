1e359d71e4859cb69624250c815fe48a6b094f59
Samuel Soares <samuelsoares179@gmail.com>
2017-05-16 21:41:23 -0300

    Adicionando anotações hibernate a classe Carro

--- Servidor/ZonaAzulDigital/src/main/java/com/zonaAzulDigital/entidades/Carro.java
+++ Servidor/ZonaAzulDigital/src/main/java/com/zonaAzulDigital/entidades/Carro.java
@@ -5,12 +5,23 @@
  */
 package com.zonaAzulDigital.entidades;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 /**
  *
  * @author Samuel
  */
-public class Carro {
-
+@Entity
+public class Carro implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @OneToOne
     private Placa placa;
 
     public Placa getPlaca() {
