ecdf8a6e6636afb1fa77d8f7972ef67911e6583b
Samuel Soares <samuelsoares179@gmail.com>
2017-05-29 00:35:11 -0300

    Metodo de Cadastrar funcionando

--- Motorista/zonaAzulDigital_Motorista/WebServiceComunication.lua
+++ Motorista/zonaAzulDigital_Motorista/WebServiceComunication.lua
@@ -1,28 +1,59 @@
 local json = require("json")
-local motorista
+
+local webService = {}
 
 
 
-local function recuperarMotorista(event)
+local function eventoCadastrarMotorista(event)
     
     if not event.isError then
         local response = json.decode(event.response)
         print(event.response)
+        print(event.status)
+
     else
-        print("Erro")
+        print("Erro") 
     end
     return
 
 end
 
-function cadastrar(motorista)
-     self.motorista = motorista
+function webService:cadastrarMotorista(motorista)
 
-local headers = {}
-headers["motorista"] = json.encode(motorista)
+	local headers = {}
 
-local params = {}
-params.headers = headers
+	headers["Content-Type"] = "application/json"
 
-network.request("https://reqres.in/api/users", "GET", recuperaMotorista, params)
+	local motoristaJson = json.encode(motorista)
+
+	local params = {}
+
+	params.headers = headers
+
+	params.body = motoristaJson
+
+	network.request("http://localhost:8084/TesteZonaAzul/rest/motorista/salvar", "POST", eventoCadastrarMotorista, params)
+
 end
+
+
+function webService:recuperarMotorista()
+
+
+	local motoristaJson = json.encode(motorista)
+
+	local params = {}
+
+	params.body = motoristaJson
+
+	network.request("http://localhost:8084/TesteZonaAzul/rest/motorista/recuperar", "GET", printEvent,params)
+
+end
+
+
+
+
+
+
+
+return webService
