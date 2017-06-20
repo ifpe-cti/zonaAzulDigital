c6aeadd1bc3004dba38011719a24bd3590b19279
jonasleal <mufireoflife@hotmail.com>
2017-06-20 00:30:21 -0300

    Correção da interface de placa

--- Servidor/ZonaAzulDigital/src/main/java/com/zonaAzulDigital/model/ModelPlaca.java
+++ Servidor/ZonaAzulDigital/src/main/java/com/zonaAzulDigital/model/ModelPlaca.java
@@ -9,12 +9,13 @@ import com.zonaAzulDigital.Excecao.DaoException;
 import com.zonaAzulDigital.entidades.Placa;
 import com.zonaAzulDigital.DAO.DaoPlacaBD;
 import com.zonaAzulDigital.interfaces.DAOPlaca;
+import com.zonaAzulDigital.interfaces.ModelPlacaInterface;
 
 /**
  *
  * @author JonasJr
  */
-public class ModelPlaca implements Model<Placa> {
+public class ModelPlaca implements ModelPlacaInterface {
 
     @Override
     public Placa cadastrar(Placa objeto) throws DaoException {
@@ -24,21 +25,17 @@ public class ModelPlaca implements Model<Placa> {
     }
 
     @Override
-    public Placa atualizar(Placa objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Placa recuperar(Placa objeto) throws DaoException {
+    public Placa recuperar(Placa placa) throws DaoException {
         DAOPlaca daoPlaca = new DaoPlacaBD();
-        objeto = (Placa) daoPlaca.recuperar(objeto.getLetras(), objeto.getNumeros());
+        placa = (Placa) daoPlaca.recuperar(placa.getLetras(), placa.getNumeros());
 
-        return objeto;
+        return placa;
     }
 
     @Override
-    public Placa deletar(Placa objeto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Placa recuperarId(int id) throws DaoException{
+        DAOPlaca dAOPlaca = new DaoPlacaBD();
+        return dAOPlaca.recuperarPorId(id);
     }
 
 }
