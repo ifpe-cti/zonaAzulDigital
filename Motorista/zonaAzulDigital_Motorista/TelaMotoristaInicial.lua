a3421133e787f423664a05a32bd755a862505a50
Samuel Soares <samuelsoares179@gmail.com>
2017-06-19 21:19:37 -0300

    Usando variavel global a tela de Motorista

--- Motorista/zonaAzulDigital_Motorista/TelaMotoristaInicial.lua
+++ Motorista/zonaAzulDigital_Motorista/TelaMotoristaInicial.lua
@@ -4,7 +4,9 @@ local scene = composer.newScene()
 
 local menu = require("Bibliotecas.menu_slider")
 
-local motorista = {}
+local widget = require("widget")
+
+
 
 local menuMotorista
 
@@ -13,19 +15,18 @@ function scene:create(event)
 
 	local sceneGroup = self.view
 
-    motorista = event.params.motorista
 
     local caixaSaldo = display.newRoundedRect(  display.contentWidth/2,  display.contentHeight/7 * 2, 170, 70, 12 )
     caixaSaldo:setFillColor( 0.2, 0.2, 1, 1 )
     local textSaldo = display.newText({text = "Seu saldo é de:", x = display.contentWidth/2,y = display.contentHeight/7  * 1.75, fontSize = 20})
-    local saldo = display.newText({text = "R$ ".. motorista.credito, x = display.contentWidth/2,y = display.contentHeight/7  * 2.1, fontSize = 20})
+    local saldo = display.newText({text = "R$ ".. motoristaLogado.credito, x = display.contentWidth/2,y = display.contentHeight/7  * 2.1, fontSize = 20})
 
     
 	menuMotorista = menu:new({
     	data={
         	{text="Inicio", scene="TelaMotoristaInicial"},
         	{text="Status", scene="scene2.lua"},
-            {text="Comprar cartão", scene="scene2.lua"},
+            {text="Comprar cartão", scene="TelaCompraMotorista"},
             {text="Relátorio de Compras", scene="scene2.lua"},
         	{text="Sair", scene="telaLogin"}
         }, 
@@ -33,10 +34,12 @@ function scene:create(event)
         	topContainerProperties={
             	bgColor={0.2, 0.2, 1, 1}, 
             	strokeColor={1,1,0.8}, 
-            	text="Bem vindo ".. motorista.nome.."!",
+            	text="Bem vindo ".. motoristaLogado.nome.."!",
                 }
             }
         })
+
+    
 
     
     
@@ -44,21 +47,22 @@ function scene:create(event)
     sceneGroup:insert(caixaSaldo)
     sceneGroup:insert(textSaldo)
     sceneGroup:insert(saldo)
+    
+
 
 end
 
-function scene:hide(event)
-     if event.phase == "will" then
-        menuMotorista:removeSelf()
-        motorista = nil
-       
-    end
+
+
+function compraCartao(event)
+
+    composer.gotoScene("TelaCompraMotorista",{ params = { motorista = motorista }})
 
 end
 
 
 scene:addEventListener("create", scene)
-scene:addEventListener("hide",scene)
+
 
 
 return scene
