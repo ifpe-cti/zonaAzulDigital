4011b9e0695a2232ea24eb2a3223557d4eb14797
Samuel Soares <samuelsoares179@gmail.com>
2017-06-21 17:08:53 -0300

    Adicionando path e metodo de verificação do guarda

--- Servidor/ZonaAzulDigital/src/main/java/com/zonaAzulDigital/services/GuardaService.java
+++ Servidor/ZonaAzulDigital/src/main/java/com/zonaAzulDigital/services/GuardaService.java
@@ -5,10 +5,21 @@
  */
 package com.zonaAzulDigital.services;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
 /**
  *
  * @author Samuel
  */
+@Path("/guarda")
 public class GuardaService {
+    
+    @GET
+    @Path("/verificaPlaca")
+    public void verificaPlaca(){
+        
+    }
+    
     
 }
