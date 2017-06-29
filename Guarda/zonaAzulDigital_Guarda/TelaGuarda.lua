13e316b5c69a1d0f296df717fd281dbe2b42b8a0
Carlos Eduardo <caducastanha@gmail.com>
2017-06-29 20:55:13 -0300

    adicionando consulta pela zona azul

--- Guarda/zonaAzulDigital_Guarda/TelaGuarda.lua
+++ Guarda/zonaAzulDigital_Guarda/TelaGuarda.lua
@@ -26,12 +26,42 @@ function scene:create()
     hifen.strokeWidth = 2.65
 
     local placa = display.newText({text = "Placa", x = display.contentWidth/2, y = display.contentHeight/4, fontSize = 25})
-    placa:setFillColor( 0, 0, 0 )
+    placa:setFillColor( 0.2, 0.2, 1, 1)
 
-    local consultar = widget.newButton({label = "consultar", labelColor = { default={ 1, 1, 1 }, over={0, 0, 0} }, x = display.contentWidth/2, y = display.contentHeight/2* 1.1, width = display.contentWidth/1.5, height = display.contentHeight/12,  shape = "roundedRect", fillColor = { default={ 0.2, 0.2, 1, 0.5 }, over={ 0.8, 0.8, 1} } })
+    local consultar = widget.newButton({label = "consultar", labelColor = { default={ 1, 1, 1 }, over={0, 0, 0} }, x = display.contentWidth/2, y = display.contentHeight/2* 1, width = display.contentWidth/1.5, height = display.contentHeight/12,  shape = "roundedRect", fillColor = { default={ 0.2, 0.2, 1, 0.5 }, over={ 0.8, 0.8, 1} } })
+
+    --cartaoConsultado.placa.letras cartaoConsultado.placa.numeros cartaoConsultado.dataEntacionamento  cartaoConsultado.numero  
+
+    local linha3 = display.newLine(display.contentWidth/10 * 1, display.contentHeight/10 * 5.8, display.contentWidth/10 * 9, display.contentHeight/10 * 5.8)
+    linha3:setStrokeColor(0.2, 0.2, 1, 1)
+    linha3.strokeWidth = 2.65
+
+    local cartaoText = display.newText({text = "CARTÃO", x = display.contentWidth/2, y = display.contentHeight/10 * 6.5, whidth = display.contentWidth/2, height = display.contentHeight/10 * 2, fontSize = 20, align = "center"})
+    cartaoText:setFillColor(0.2, 0.2, 1, 1)
+    local numeroText = display.newText({text = "Numero: 1", x = display.contentWidth/2, y = display.contentHeight/10 * 7.5, whidth = display.contentWidth/2, height = display.contentHeight/10 * 2, fontSize = 20, align = "left"})
+    
+    local placaText = display.newText({text = "Placa: KKZ-2443", x = display.contentWidth/2, y = display.contentHeight/10 * 8.5,whidth = display.contentWidth/2, height = display.contentHeight/10 * 2, fontSize = 20, align = "left"})
+    
+    local dataText = display.newText({text = "Horário: 14:35", x = display.contentWidth/2, y = display.contentHeight/10 * 9.5, whidth = display.contentWidth/2, height = display.contentHeight/10 * 2, fontSize = 20, align = "left"})
+   
+    
+
+    if cartaoConsultado.letras ~= nil then
+        
+        numeroText = numeroText .. cartaoConsultado.numero
+
+        placaText = placaText .. cartaoConsultado.letras .. "-" .. cartaoConsultado.numeros
+
+        dataText = dataText .. cartaoConsultado.dataEntacionamento
+        
+
+    end
 
     consultar:addEventListener("touch", consultarPlaca)
-
+    
+    sceneGroup:insert(numeroText)
+    sceneGroup:insert(placaText)
+    sceneGroup:insert(dataText)
     sceneGroup:insert(linha1)
     sceneGroup:insert(linha2)
     sceneGroup:insert(hifen)
