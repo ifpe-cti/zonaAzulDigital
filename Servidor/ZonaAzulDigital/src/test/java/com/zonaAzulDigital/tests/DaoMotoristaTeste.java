c8259f1a395c22d145c6387f2f3304ae5edfbf42
Carlos Eduardo <caducastanha@gmail.com>
2017-07-05 23:25:42 -0300

    adicionando teste da cadastro

---
+++ Servidor/ZonaAzulDigital/src/test/java/com/zonaAzulDigital/tests/DaoMotoristaTeste.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zonaAzulDigital.tests;
+
+
+import com.zonaAzulDigital.DAO.DaoMotoristaBD;
+import com.zonaAzulDigital.Excecao.DaoException;
+import com.zonaAzulDigital.entidades.Motorista;
+import com.zonaAzulDigital.interfaces.DAOMotorista;
+import java.math.BigDecimal;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author Samuel
+ */
+public class DaoMotoristaTeste {
+    public DAOMotorista dmbd;
+    
+    public Motorista m;
+    
+    @Before
+    public void before(){
+        dmbd = new DaoMotoristaBD();
+        m = new Motorista(0, "Carlos Eduardo", "04982857407", BigDecimal.ZERO, "carlos");
+    }
+    @Test
+    public void testandoCadastroMostorista() throws DaoException{
+        Motorista mc = dmbd.cadastrar(m);
+        
+         assertEquals("Carlos Eduardo",mc.getNome());
+         
+    }
+    
+}
