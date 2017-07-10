775458247de6f859a8d92233e50e09ebbe1c8fc5
Carlos Eduardo <caducastanha@gmail.com>
2017-07-10 11:36:45 -0300

    modificando tela do guarda

--- Guarda/zonaAzulDigital_Guarda/TelaGuarda.lua
+++ Guarda/zonaAzulDigital_Guarda/TelaGuarda.lua
@@ -40,30 +40,37 @@ function scene:create()
     local placa = display.newText({text = "Placa", x = display.contentWidth/2, y = display.contentHeight/10 * 2.75, fontSize = 25})
     placa:setFillColor( 0.2, 0.2, 1, 1)
 
-    local consultar = widget.newButton({label = "Consultar", labelColor = { default={ 1, 1, 1 }, over={0, 0, 0} }, x = display.contentWidth/2, y = display.contentHeight/2* 1, width = display.contentWidth/1.5, height = display.contentHeight/12,  shape = "roundedRect", fillColor = { default={ 0.2, 0.2, 1, 0.5 }, over={ 0.8, 0.8, 1} } })
+    local consultar = widget.newButton({label = "CONSULTAR", labelColor = { default={ 1, 1, 1 },  over={0, 0, 0} },fontSize = 14 , x = display.contentWidth/2, y = display.contentHeight/2* 1, width = display.contentWidth/1.5, height = display.contentHeight/12,  shape = "roundedRect", fillColor = { default={ 0.2, 0.2, 1, 0.5 }, over={ 0.8, 0.8, 1} } })
 
     --cartaoConsultado.placa.letras cartaoConsultado.placa.numeros cartaoConsultado.dataEntacionamento  cartaoConsultado.numero  
 
-    local linha3 = display.newLine(display.contentWidth/10 * 1, display.contentHeight/10 * 5.8, display.contentWidth/10 * 9, display.contentHeight/10 * 5.8)
-    linha3:setStrokeColor(0.2, 0.2, 1, 1)
-    linha3.strokeWidth = 2.65
+    
+    if cartaoConsultado.letras ~= nil then
+        local linha3 = display.newLine(display.contentWidth/10 * 1, display.contentHeight/10 * 5.8, display.contentWidth/10 * 9, display.contentHeight/10 * 5.8)
+        linha3:setStrokeColor(0.2, 0.2, 1, 1)
+        linha3.strokeWidth = 2.65
 
-    local cartaoText = display.newText({text = "CARTÃO", x = display.contentWidth/2, y = display.contentHeight/10 * 6.5, whidth = display.contentWidth/2, height = display.contentHeight/10 * 2, fontSize = 20, Textalign = "center"})
-    cartaoText:setFillColor(0.2, 0.2, 1, 1)
-    numeroText = display.newText({text = "Número: ", x = display.contentWidth/2, y = display.contentHeight/10 * 7.5, whidth = display.contentWidth/10 * 7, height = display.contentHeight/10 * 2, fontSize = 20, Textalign = "left"})
+
+        local cartaoText = display.newText({text = "CARTÃO", x = display.contentWidth/2, y = display.contentHeight/10 * 6.5, whidth = display.contentWidth/2, height = display.contentHeight/10 * 2, fontSize = 20, Textalign = "center"})
+        cartaoText:setFillColor(0.2, 0.2, 1, 1)
+        numeroText = display.newText({text = "Número: ".. cartaoConsultado.numero, x = display.contentWidth/2, y = display.contentHeight/10 * 7.5, whidth = display.contentWidth/10 * 7, height = display.contentHeight/10 * 2, fontSize = 20, Textalign = "left"})
     
-    placaText = display.newText({text = "Placa: ", x = display.contentWidth/2, y = display.contentHeight/10 * 8.5,whidth = display.contentWidth/10 * 7, height = display.contentHeight/10 * 2, fontSize = 20, Textalign = "left"})
+        placaText = display.newText({text = "Placa: ".. cartaoConsultado.placa.letras .. "-" .. cartaoConsultado.placa.numeros, x = display.contentWidth/2, y = display.contentHeight/10 * 8.5,whidth = display.contentWidth/10 * 7, height = display.contentHeight/10 * 2, fontSize = 20, Textalign = "left"})
     
-    dataText = display.newText({text = "Tempo Restante: ", x = display.contentWidth/2, y = display.contentHeight/10 * 9.5, whidth = display.contentWidth/10 * 7, height = display.contentHeight/10 * 2, fontSize = 20, Textalign = "left"})
-   
+        dataText = display.newText({text = "Restante: " .. cartaoConsultado.dataFim, x = display.contentWidth/2, y = display.contentHeight/10 * 9.5, whidth = display.contentWidth/10 * 7, height = display.contentHeight/10 * 2, fontSize = 20, Textalign = "left"})
+        
+        sceneGroup:insert(cartaoText)
+        sceneGroup:insert(numeroText)
+        sceneGroup:insert(placaText)
+        sceneGroup:insert(dataText)
+        sceneGroup:insert(linha3)
+   end
     consultar:addEventListener("touch", consultarPlaca)
 
 
     sceneGroup:insert(iconeTxt)
     sceneGroup:insert(iconeCar)
-    sceneGroup:insert(numeroText)
-    sceneGroup:insert(placaText)
-    sceneGroup:insert(dataText)
+  
     sceneGroup:insert(linha1)
     sceneGroup:insert(linha2)
     sceneGroup:insert(hifen)
@@ -82,16 +89,7 @@ function scene:show(event)
         numeros.align = "center"
         numeros.inputType = "number"
 
-        if cartaoConsultado.letras ~= nil then
-        
-            numeroText.text = numeroText.text .. cartaoConsultado.numero
-
-            placaText.text = placaText.text .. cartaoConsultado.placa.letras .. "-" .. cartaoConsultado.placa.numeros
-
-            dataText.text = dataText.text .. cartaoConsultado.dataFim
-        
-
-        end     
+     
     end
 end
 
