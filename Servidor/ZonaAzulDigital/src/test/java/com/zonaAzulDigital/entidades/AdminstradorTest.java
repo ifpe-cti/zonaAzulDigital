5828e10eecbe7847cec9c8fe258db3814ff3f9ef
Danielgol <danielgol@yahoo.com.br>
2017-11-23 20:01:06 -0300

    Adicionando Testes JUnit de Adminstrador: nome normal com espaço; nome nulo;

---
+++ Servidor/ZonaAzulDigital/src/test/java/com/zonaAzulDigital/entidades/AdminstradorTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zonaAzulDigital.entidades;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Daniel Carneiro Rosa
+ */
+public class AdminstradorTest {
+    
+    @Test
+    public void testGetPrimeiroNome_NomeComEspaco() {
+    
+        Adminstrador admin = new Adminstrador();
+
+        admin.setNome("Daniel Carneiro Rosa");
+
+        String pNome = admin.getPrimeiroNome();
+        
+        assertEquals(pNome, "Daniel");
+        
+    }
+    
+    @Test
+    public void testGetPrimeiroNome_NomeNull() {
+        
+        Adminstrador admin = new Adminstrador();
+
+        admin.setNome(null);
+        
+        String pNome = admin.getPrimeiroNome();
+        
+        assertEquals(pNome, "");
+        
+    }
+    
+}
